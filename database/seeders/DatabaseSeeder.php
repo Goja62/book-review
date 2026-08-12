131b8101598ceaad0614a4cb781805906cae9eb3
Goja <gortnar@beograd.com>
2026-08-12 09:24:02 +0200

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Book;
+use App\Models\Review;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        Book::factory(33)->create()->each(function ($book) {
+            $numRevies = random_int(5, 30);
+
+            Review::factory()->count($numRevies)->good()->for($book)->create();
+        });
+
+        Book::factory(33)->create()->each(function ($book) {
+            $numRevies = random_int(5, 30);
+
+            Review::factory()->count($numRevies)->average()->for($book)->create();
+        });
+
+        Book::factory(34)->create()->each(function ($book) {
+            $numRevies = random_int(5, 30);
+
+            Review::factory()->count($numRevies)->bad()->for($book)->create();
+        });
+    }
+}
