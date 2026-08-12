1c12a288f678b322a7e6cbeeab37dfa65c63043a
Goja <gortnar@beograd.com>
2026-08-12 17:09:28 +0200

    Highest Rated & Popular Books

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,9 +3,9 @@
 namespace App\Models;
 
 use App\Models\Review;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Query\Builder;
 
 class Book extends Model
 {
@@ -19,5 +19,15 @@ class Book extends Model
     public function scopeTitle(Builder $query, string $title): Builder
     {
         return $query->where('title', 'LIKE', '%' . $title . '%');
+    }
+
+    public function scopePopular(Builder $query): Builder
+    {
+        return $query->withCount('reviews')->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query): Builder
+    {
+        return $query->withAvg('reviews', 'rating')->orderBy('reviews_avg_rating', 'desc');
     }
 }
